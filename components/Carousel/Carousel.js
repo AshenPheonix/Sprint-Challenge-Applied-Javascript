5dd64da3cfd6ac9f0142faf7c65f054718dc81ef
Brandon Porter <brendlorend@gmail.com>
2019-05-17 08:43:03 -0700

    Stretch: initial infinite carousel finished

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,9 +1,44 @@
 class Carousel {
+    constructor(root){
+        this.root=root
+        this.currentIndex=0
+        this.imageList=this.root.querySelectorAll('img')
+        this.leftButton=this.root.querySelector('.left-button')
+        this.rightButton=this.root.querySelector('.right-button')
 
+        this.display()
+
+        this.leftButton.addEventListener('click',(e)=>{
+                this.moveLeft()
+        })
+        this.rightButton.addEventListener('click',(e)=>{
+                this.moveRight()
+        })
+    }
+
+    display(){
+       this.imageList[this.currentIndex].style.display='block'
+    }
+    hide(){
+        this.imageList[this.currentIndex].style.display='none'
+    }
+
+    moveLeft(){
+        this.hide()
+        if(--this.currentIndex<0)
+            this.currentIndex=this.imageList.length-1
+        this.display()
+    }
+    moveRight(){
+        this.hide()
+        if(++this.currentIndex>=this.imageList.length)
+            this.currentIndex=0
+        this.display()
+    }
 }
 
-let carousel = document.querySelector();
-
+let carousel = document.querySelector('.carousel');
+new Carousel(carousel)
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
     2. You will need to grab a reference to all of the images
